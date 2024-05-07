22461b3c64e1a5d93f4ccc3b511febded90bf50d
James Huston <james@jameshuston.net>
2024-05-07 13:50:48 -0400

    Some initial tweaks to try to get this to compile

--- src/AuctionatorBase.h
+++ src/AuctionatorBase.h
@@ -5,6 +5,40 @@
 #include "AuctionatorBase.h"
 #include "Log.h"
 
+#ifdef TRINITYCORE_LOG_H
+
+#define LOG_TRACE(filterType__, message__, ...) \
+    TC_LOG_TRACE(filterType__, "{}", message__, ## __VA_ARGS__)
+
+#define LOG_DEBUG(filterType__, message__, ...) \
+    TC_LOG_DEBUG(filterType__, "{}", message__, ## __VA_ARGS__)
+
+#define LOG_INFO(filterType__, message__, ...)  \
+    TC_LOG_INFO(filterType__, "{}", message__, ## __VA_ARGS__)
+
+#define LOG_WARN(filterType__, message__, ...)  \
+    TC_LOG_WARN(filterType__, "{}", message__, ## __VA_ARGS__)
+
+#define LOG_ERROR(filterType__, message__, ...) \
+    TC_LOG_ERROR(filterType__, "{}", message__, ## __VA_ARGS__)
+
+#define LOG_FATAL(filterType__, message__, ...) \
+    TC_LOG_FATAL(filterType__, "{}", message__, ## __VA_ARGS__)
+
+enum AuctionHouseType
+{
+    AUCTIONHOUSE_NEUTRAL = 0,
+    AUCTIONHOUSE_ALLIANCE = 1,
+    AUCTIONHOUSE_HORDE = 2
+};
+
+struct AuctionEntry : AuctionPosting
+{
+
+};
+
+#endif
+
 class AuctionatorBase
 {
     private:
